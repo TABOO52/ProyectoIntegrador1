2d28581409bc8c7a6b3dc34c3291f5de0163abec
TABOO_LOCAL <fate8853@gmail.com>
2024-06-16 19:12:16 -0500

    Se realiza escrapeo de los titulos

--- prueba.py
+++ prueba.py
@@ -6,5 +6,12 @@ import pandas as pd
 
 url = "https://cuspide.com/100-mas-vendidos/"
 response = requests.get(url)
-print(response.text)
+
+libros = bs(response.content, "html.parser")
+titulo = libros.find_all(class_="name product-title woocommerce-loop-product__title")
+
+for i in titulo:
+    print(i.get_text())
+
+
 
