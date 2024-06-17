25af3e347d612625f67edacb18189692427c613c
TABOO_LOCAL <fate8853@gmail.com>
2024-06-16 20:10:11 -0500

    Se realiza manejo de excepciones

--- prueba.py
+++ prueba.py
@@ -9,9 +9,28 @@ response = requests.get(url)
 
 libros = bs(response.content, "html.parser")
 titulo = libros.find_all(class_="name product-title woocommerce-loop-product__title")
+precio = libros.find_all(class_="price")
+url_libros = []
+precio_libros = []
+titulo_libros = []
 
-for i in titulo:
-    print(i.get_text())
+url_error = []
+titulo_error = []
 
+for i , j in zip(precio,titulo):
+    try:
+        titulo_ = j.find("a").get_text()
+        url_ = j.find("a").get("href")
+        precio_ = i.find("bdi").get_text(strip = True).replace("$","").replace(".","").replace(",",".")  
+        
+        titulo_libros.append(titulo_)
+        url_libros.append(url_)
+        precio_libros.append(float(precio_))
+    except AttributeError:
+        url_error.append(url_)
+        titulo_error.append(titulo_)
 
-
+print(precio_libros)
+print(url_libros)
+print(url_error)
+print(titulo_libros)
